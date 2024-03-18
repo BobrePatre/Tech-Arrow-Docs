a1d62b956794bfcb10e067d4404f1c07bc10391a
Ge0rg25 <Ge0rgiusGus@yandex.ru>
2024-03-18 15:39:42 +0300

    ⚠️debugging(code generation): debug and fix gh workflow for generating libraries

--- scripts/delete-repo.sh
+++ scripts/delete-repo.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 repo_name=$1
-owner="YOUR_GITHUB_USERNAME_OR_ORG" # Adjust this to your username or organization
+owner="BobrePatre" # Adjust this to your username or organization
 
 curl \
   -X DELETE \
   -H "Authorization: token $GH_PAT" \
   -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/$owner/$repo_name
+  https://api.github.com/repos/$owner/"$repo_name"
 
 echo "Repository $repo_name has been deleted."
