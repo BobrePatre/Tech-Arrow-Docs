e3b75f754eb4396fcb6b681bd7d7379b078df1f6
Ge0rg25 <Ge0rgiusGus@yandex.ru>
2024-03-18 19:46:17 +0300

    ⚠️debug(debugging github actions):

--- scripts/delete-repo.sh
+++ scripts/delete-repo.sh
@@ -2,10 +2,12 @@
 
 repo_name=$1
 owner="BobrePatre" # Adjust this to your username or organization
-GH_PAT="your_github_pat_here" # Make sure to replace this with your actual GitHub Personal Access Token
+GH_PAT="${GH_PAT}" # Make sure to replace this with your actual GitHub Personal Access Token
 
 # Check if the repository exists
 status_code=$(curl -o /dev/null -s -w "%{http_code}\n" -H "Authorization: token $GH_PAT" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$owner/"$repo_name")
+
+echo "$status_code is status code of repo"
 
 if [ "$status_code" -eq 200 ]; then
   # The repository exists, proceed with deletion
